1804cdd211aeea7bc0c8c218cbbd747d0d0e89f7
Patrick <patrick.bethke@augmented-robotics.com>
2022-08-10 13:05:00 +0200

    initial commit

---
+++ joystickView/build/generated/source/buildConfig/debug/com/zerokol/views/joystickView/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zerokol.views.joystickView;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.zerokol.views.joystickView";
+  public static final String BUILD_TYPE = "debug";
+}
